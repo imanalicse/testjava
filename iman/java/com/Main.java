60d4b5cf7daae64ed44bc8d0820d575383c8ffc3
iman <imanali.cse@gmail.com>
2020-02-26 22:12:44 +0600

    Initial commit

---
+++ iman/java/com/Main.java
@@ -0,0 +1,18 @@
+package iman.java.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        if (person != null) {
+
+        }
+    }
+}
+
+class Person {
+    public void printName() {
+
+    }
+}
